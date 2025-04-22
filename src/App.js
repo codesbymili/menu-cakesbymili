77c199899af9b38dbfa73971e1e1e07573e4eac0
milin_michael <milin.michael@kalkitech.com>
2025-04-23 00:08:23 +0530

    menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import cakes from './cakeData';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Content></Content>
     </div>
   );
 }
 
-export default App;
+function Header(){
+  return(
+    <div className='header'>
+      <img className='logo' src="/images/logo.png"/>
+    </div>
+  );
+}
+
+function Content(){
+  const[selectedItem, setSelectedItem] = useState(cakes[0]);
+
+  function handleSelection(cake){
+    setSelectedItem(selected => selected = cake);
+  }
+
+    return(
+    <div className='content'>
+      <Menu selectedItem={selectedItem} handleSelection={handleSelection}></Menu>
+      <Details selectedItem={selectedItem} handleSelection={handleSelection}></Details>
+    </div>
+  );
+}
+
+function Menu({selectedItem, handleSelection}){
+  
+  return(
+    <div className='menu'>
+      <h3>Menu</h3>
+      <Items selectedItem={selectedItem} handleSelection={handleSelection}></Items>
+    </div>
+  );
+}
+
+function Items({selectedItem, handleSelection}){
+  return(
+    <div className='items'>
+      {cakes.map((cake) => (
+          <div key={cake.id} className={selectedItem?.id === cake.id ? 'item-card item-selected' : 'item-card' } onClick={() => handleSelection(cake)}>
+            <img src={cake.image} alt="" className='cake-image' />
+            <p>{cake.name}</p>
+          </div>))}
+    </div>
+  );
+}
+
+function Button({children}){
+  return(
+    <button className='button'>{children}</button>
+  );
+
+}
+
+function Details({selectedItem, handleSelection}){
+  return(
+    <div className='details'>
+        <h3>{selectedItem?.name}</h3>
+        <img src={selectedItem?.image} alt="" />
+        <p className='price'>Price : Rs.{selectedItem?.price}/Kg</p>
+        <p className='description'><em>" {selectedItem?.description}"</em></p>
+    </div>
+  );
+}
---
+++ src/cakeData.js
@@ -0,0 +1,84 @@
+import cake from "./assets/cake.jpg"
+
+const cakes = [
+    {
+      id: 1,
+      name: "Choco-Galaxy Cake",
+      image: cake,
+      price: 895,
+      description: "Moist Chocolate cake layered with chocolate cream, chocolate chips and chocolate ganache"
+    },
+    {
+      id: 2,
+      name: "Butterscotch Praline",
+      image: cake,
+      price: 985,
+      description: "Butterscotch cake layered with butterscotch cream, caramel and crunchy praline of nuts"
+    },
+    {
+      id: 3,
+      name: "Vanilla Cream Cake",
+      image: cake,
+      price: 825,
+      description: "Classic Vanilla Cake layered with simple velvety vanilla cream and white chocolate chips"
+    },
+    {
+      id: 4,
+      name: "Choco Caramel",
+      image: cake,
+      price: 995,
+      description: "Rich chocolate cake with velvety ganache adorned with silky caramel mousse"
+    },
+    {
+      id: 5,
+      name: "Spanish Delight Cake",
+      image: cake,
+      price: 985,
+      description: "Spanish delight flavoured cream layered in moist chocolate cake with crunch of praline nuts"
+    },
+    {
+      id: 6,
+      name: "Choco Caramel Crunch",
+      image: cake,
+      price: 1125,
+      description: "Rich chocolate cake with velvety ganache adorned with silky caramel mousse and crunchy nuts"
+    },
+    {
+      id: 7,
+      name: "Vanilla Crunchy",
+      image: cake,
+      price: 880,
+      description: "Vanilla cake layered with special vanilla frosting and crunchy nuts praline"
+    },
+    {
+      id: 8,
+      name: "Butterscotch Caramel Cake",
+      image: cake,
+      price: 925,
+      description: "Butterscotch cake layered with butterscotch frosting and rich caramel sauce"
+    },
+    {
+      id: 9,
+      name: "Double Chocolate Caramel Praline",
+      image: cake,
+      price: 1100,
+      description: "Moist chocolate and butterscotch cake layers with chocolate ganache and rich butterscotch caramel sauce with crunchy praline"
+    },
+    {
+      id: 10,
+      name: "Brownie Tower Cake",
+      image: cake,
+      price: 880,
+      description: "16 pieces brownie tower consisting of double chocolate fudge brownies, chocolate truffles and chocolate sprinkles"
+    },
+    {
+      id: 11,
+      name: "Chocolate Dream Cake",
+      image: cake,
+      price: 1500,
+      description: "5 layered torte cake with chocolate cake base, chocolate mousse, dark chocolate ganache, che chips, milk chocolate and sprinkle of coco powder"
+    }
+  ];
+  
+
+  export default cakes;
